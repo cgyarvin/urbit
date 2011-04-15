6103f1b997f5f1393f761a7f403911d3f002a0c4
C. Guy Yarvin <cgyarvin@gmail.com>
2011-04-15 12:39:26 -0700

    Preparing for metal/fire refactor.

--- gen269/6/ut_fire.c
+++ gen269/6/ut_fire.c
@@ -159,6 +159,6 @@
 */
   u2_ho_jet 
   j2_mcj(Pit, ut, fire)[] = {
-    { ".3", c3__hevy, j2_mc(Pit, ut, fire), u2_jet_test, u2_none, u2_none },
+    { ".3", c3__hevy, j2_mc(Pit, ut, fire), Tier6_b, u2_none, u2_none },
     { }
   };
